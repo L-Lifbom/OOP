11012f17de5e5e7df6513e3dde81c7b81da94f59
L-Lifbom <141008398+L-Lifbom@users.noreply.github.com>
2024-02-26 12:34:38 +0100

    step 3 fin

--- 2-OOP-Poker/script.js
+++ 2-OOP-Poker/script.js
@@ -32,6 +32,10 @@ class Deck {
         }
     }
 
+    thrownCards () {
+        cardPile = []
+    }
+
 }
 
 class Player {
@@ -51,6 +55,26 @@ class Player {
             return total + current.value
         }, 0)
     }
+
+    placeCards(a) {
+        for (let i = 0; i < a; i++) {
+            cardPile.push(this.hand[this.hand.length - 1])
+            this.hand.pop()
+        }
+
+        for (let i = 0; i < a; i++) {
+            this.hand.push(deck.deck.pop())
+        }
+    }
+
+    placeAllCards(a) {
+        for (let i = 0; i < a; i++) {
+            cardPile.push(this.hand.a)
+            this.hand.pop()
+        }
+
+
+    }
 }
 
 const deck = new Deck()
@@ -64,4 +88,17 @@ player2.dealCards()
 
 console.log(`Player 1: ${player1.playerName} has these cards:\n${player1.hand.map(card =>`${card.name} of ${card.suit}`).join(',\n')} \nWith a total value of ${player1.calculateCards()}`)
 console.log(`Player 2: ${player2.playerName} has these cards:\n${player2.hand.map(card =>`${card.name} of ${card.suit}`).join(',\n')} \nWith a total value of ${player2.calculateCards()}`)
-console.log("Remaining deck: ", deck)
+console.log("Remaining cards in deck: ", deck.deck.length)
+
+player1.placeCards(2)
+player2.placeCards(2)
+console.log(`${player1.playerName} threw and drew two new cards:\n${player1.hand.map(card =>`${card.name} of ${card.suit}`).join(',\n')} \nWith a total value of ${player1.calculateCards()}`)
+console.log(`${player2.playerName} threw and drew two new cards:\n${player2.hand.map(card =>`${card.name} of ${card.suit}`).join(',\n')} \nWith a total value of ${player2.calculateCards()}`)
+console.log("Remaining cards in deck: ", deck.deck.length)
+
+player1.placeAllCards(player1.hand.length)
+player2.placeAllCards(player2.hand.length)
+
+console.log(player1)
+console.log(player2)
+console.log("Remaining cards in deck: ", deck.deck.length)
--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # OOP
 
 ## Description
+Color selector and poker game created using Object Oriented Programming (OOP)
 
 ## Visuals
+
 
 ## Installation
 - Clone this project to your preferred directory.
 - Install code editor extension "Live Server"
 - Click "Go Live" in the bottom right corner of the code editor, this will activate the live server.
-- If it does not work: Copy the local path into a web browser, ex Google Chrome & Firefox usally it is `http://127.0.0.1:5500/`
+- If it does not work: Copy the local path `http://127.0.0.1:5500/` into a web browser, ex Google Chrome & Firefox
 - Enjoy!
 
 ## Usage
+- Prototype Based: Creates and prints rgb, hex, rgba colors in console.
+- OOP Poker: Deals and throws cards between two players in console.
 
 ## Contact Information
+- Github: https://github.com/L-Lifbom
